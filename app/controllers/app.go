7d35871a27d29e0b0899af2712b12c6e06c96be5
jjongs08 <jjongs08@gmail.com>
2015-06-28 02:30:44 +0900

    add database connection sample

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,6 +2,10 @@ package controllers
 
 import (
 	"github.com/revel/revel"
+	"database/sql"
+	"fmt"
+	"strconv"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type App struct {
@@ -9,6 +13,54 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
+	db, err := sql.Open("mysql", "root:@/revel")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close() // 関数がリターンする直前に呼び出される
+
+//	CREATE TABLE
+	query := `
+		CREATE TABLE test (
+		  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+		  title varchar(255) DEFAULT NULL,
+		  PRIMARY KEY (id)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+	`
+	_, err = db.Exec(query)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+//	INSERT
+	for i := 1; i <= 20; i++ {
+		title := "test" + strconv.Itoa(i)
+		_, err = db.Exec(`INSERT test (title) VALUES (?)`, title)
+	}
+	if err != nil {
+		panic(err.Error())
+	}
+
+//	SELECT
+	rows, err := db.Query("SELECT id, title FROM test")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer rows.Close()
+	fmt.Println(rows)
+	for rows.Next() {
+		var ID int
+		var post_title string
+		if err := rows.Scan(&ID, &post_title); err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(ID, post_title)
+	}
+
+
+
   	greeting:="Aloha World"
 	return c.Render(greeting)
 }
